5558efd2ee2037cf6b51dbb8855f2d2f644f213b
Thomas Kyaw <thomas.kyaw.69@gmail.com>
2026-04-01 20:38:57 +0800

    type Book added

---
+++ types/book.ts
@@ -0,0 +1,11 @@
+export interface Book {
+    id: number;
+    title: string;
+    author: string;
+    genre: string;
+    description: string;
+    image: string;
+    isbn: string;
+    published: string;
+    publisher: string;
+}
